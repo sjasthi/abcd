f239ef9b5ec28a0aa46603f0a47763b586d2f50c
Ishana Didwania <48626979+iDidwania@users.noreply.github.com>
2020-02-15 09:42:18 -0800

    create_dress.php and create_the_dress.php are now linked to one another. I will start the form validation and adding the new dress.

--- create_the_dress.php
+++ create_the_dress.php
@@ -15,56 +15,58 @@ if (isset($_POST['topic'])){
     $imageName = basename($_FILES["fileToUpload"]["name"]);
     $validate = true;
     $validate = emailValidate($answer);
+
+    echo "SUCCESS";
     
     
-    if($validate){
+//     if($validate){
         
-        $target_dir = "Images/$topic/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        // Check if image file is a actual image or fake image
-        if(isset($_POST["submit"])) {
-            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-            if($check !== false) {
-                $uploadOk = 1;
-            } else {
-                header('location: createQuestion.php?createQuestion=fileRealFailed');
-                $uploadOk = 0;
-            }
-        }
-        // Check if file already exists
-        if (file_exists($target_file)) {
-            header('location: createQuestion.php?createQuestion=fileExistFailed');
-            $uploadOk = 0;
-        }
+//         $target_dir = "Images/$topic/";
+//         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+//         $uploadOk = 1;
+//         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+//         // Check if image file is a actual image or fake image
+//         if(isset($_POST["submit"])) {
+//             $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+//             if($check !== false) {
+//                 $uploadOk = 1;
+//             } else {
+//                 header('location: create_dres.php?create_dress=fileRealFailed');
+//                 $uploadOk = 0;
+//             }
+//         }
+//         // Check if file already exists
+//         if (file_exists($target_file)) {
+//             header('location: create_dress.php?create_dress=fileExistFailed');
+//             $uploadOk = 0;
+//         }
         
-        // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-            header('location: createQuestion.php?createQuestion=fileTypeFailed');
-            $uploadOk = 0;
-        }
-        // Check if $uploadOk is set to 0 by an error
-        if ($uploadOk == 0) {
+//         // Allow certain file formats
+//         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//         && $imageFileType != "gif" ) {
+//             header('location: create_dress.php?create_dres=fileTypeFailed');
+//             $uploadOk = 0;
+//         }
+//         // Check if $uploadOk is set to 0 by an error
+//         if ($uploadOk == 0) {
             
-        // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//         // if everything is ok, try to upload file
+//         } else {
+//             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                 
-                $sql = "INSERT INTO questions(topic,question,choice_1,choice_2,choice_3,choice_4,answer,image_name)
-                VALUES ('$topic','$question','$choice1','$choice2','$choice3','$choice4','$answer','$target_file')
-                ";
+//                 $sql = "INSERT INTO questions(topic,question,choice_1,choice_2,choice_3,choice_4,answer,image_name)
+//                 VALUES ('$topic','$question','$choice1','$choice2','$choice3','$choice4','$answer','$target_file')
+//                 ";
 
-                mysqli_query($db, $sql);
-                header('location: questions_list.php?createQuestion=Success');
-                }
-            }
-        }else{
-            header('location: createQuestion.php?createQuestion=answerFailed'); 
-    }        
+//                 mysqli_query($db, $sql);
+//                 header('location: questions_list.php?createQuestion=Success');
+//                 }
+//             }
+//         }else{
+//             header('location: createQuestion.php?createQuestion=answerFailed'); 
+//     }        
 
-}//end if
+ }//end if
 
 function emailValidate($answer){
     global $choice1,$choice2,$choice3,$choice4;
