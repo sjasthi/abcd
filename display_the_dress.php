a07735020a8a01cae123a5e8b4bcb4f572593f8d
ssmriti13 <48972435+ssmriti13@users.noreply.github.com>
2020-03-13 22:13:36 -0500

    Update display_the_dress.php

--- display_the_dress.php
+++ display_the_dress.php
@@ -2,19 +2,16 @@
 
 <style>
 .head {
-  text-align: center;
   font-family: "Times New Roman";
   color: rgb(200,55,0);
 }
 
 .title {
-  text-align: center;
   font-family: "Times New Roman";
   color: rgb(0,200,55);
 }
 
 .words{
-  text-align: center;
   font-family: "Times New Roman";
 }
 </style>
@@ -29,13 +26,24 @@ if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($db, $_GET['id']);
     $sql = "SELECT * FROM `dresses` WHERE id = " . $id;
     $GLOBALS['row_data'] = mysqli_query($db, $sql);
+} 
+else if(isset($_GET['name'])) {
+
+    $name = mysqli_real_escape_string($db, $_GET['name']);
+    $sql = "SELECT * FROM `dresses` WHERE name = " . $name;
+    $GLOBALS['row_data'] = mysqli_query($db, $sql);
 }
+
 if ($row_data->num_rows > 0) {
     // fetch row_data from $_Globals
     while($row = $row_data->fetch_assoc()) {
       print( '<h1 class= "head">'.$row["name"]. '</h1>
               <h3 class= "title"> Description: </h3><p class= "words" >'.$row["description"]. '</p>
-              <h3 class= "title"> Did You Know? </h3><p class= "words">' .$row["did_you_know"]. '</p>');
+              <h3 class= "title"> Did You Know? </h3><p class= "words">' .$row["did_you_know"]. '</p>
+              <h3 class= "title"> Category </h3><p class= "words">' .$row["category"]. '</p>
+              <h3 class= "title"> Type </h3><p class= "words">' .$row["type"]. '</p>
+              <h3 class= "title"> State Name </h3><p class= "words">' .$row["state_name"]. '</p>
+              <h3 class= "title"> Key Words </h3><p class= "words">' .$row["key_words"]. '</p>');
     }
 } else {
   print('no data');
