d041cb4561213239348661c35efa40d4b8c9bd24
vvundama <vishnuvunda@gmail.com>
2020-03-20 18:41:24 -0500

    Corrected the "where"

--- modify_the_preferences.php
+++ modify_the_preferences.php
@@ -8,7 +8,7 @@ if (isset($_POST['new_rows'])){
     $questions = mysqli_real_escape_string($db, $_POST['new_questions']);
     $dress = mysqli_real_escape_string($db, $_POST['new_dress']);
       
-    $sql1 = "UPDATE `preferences` SET `value`= $rows WHERE `name` = 'NO_OF_TOPICS_PER_ROW'";
+    $sql1 = "UPDATE `preferences` SET `value`= $rows WHERE `name` = 'NO_OF_DRESSES_PER_ROW'";
 
     $sql2 = "UPDATE `preferences` SET `value`= $questions WHERE `name` = 'NO_OF_DRESSES_TO_DISPLAY'";
 
