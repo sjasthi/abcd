f0d079f4a777a85f93afd651b7351f9026e64998
vvundama <vishnuvunda@gmail.com>
2020-03-21 09:40:38 -0500

    changed name to comments

--- modify_the_preferences.php
+++ modify_the_preferences.php
@@ -2,21 +2,27 @@
 //edited by Vishnu
 include_once 'db_configuration.php';
 
-if (isset($_POST['new_rows'])){
+if (isset($_POST['row_count'])){
+    $rows = mysqli_real_escape_string($db, $_POST['row_count']);
+}
 
-    $rows = mysqli_real_escape_string($db, $_POST['new_rows']);
-    $questions = mysqli_real_escape_string($db, $_POST['new_questions']);
-    $dress = mysqli_real_escape_string($db, $_POST['new_dress']);
-      
+if (isset($_POST['dresses_count'])){
+    $questions = mysqli_real_escape_string($db, $_POST['dresses_count']);
+}
+
+if (isset($_POST['fav_dress'])){
+    $dress = mysqli_real_escape_string($db, $_POST['fav_dress']);
+}
+ 
     $sql1 = "UPDATE `preferences` SET `value`= $rows WHERE `name` = 'NO_OF_DRESSES_PER_ROW'";
 
     $sql2 = "UPDATE `preferences` SET `value`= $questions WHERE `name` = 'NO_OF_DRESSES_TO_DISPLAY'";
 
-    $sq13 = "UPDATE `preferences` SET `value`= $dress WHERE `name` = 'NAME_OF_FAVORITE_DRESS'";
+    $sq13 = "UPDATE `preferences` SET `value`= $dress WHERE `comments` = 'NAME_OF_FAVORITE_DRESS'";
 
     mysqli_query($db, $sql1);
     mysqli_query($db, $sql2);
     mysqli_query($db, $sql3);
     header('location: index.php?preferencesUpdated=Success');
-}//end if
+
 ?>
--- preferences.php
+++ preferences.php
@@ -8,7 +8,7 @@ include('header.php');
 //This file is going to be edited by Vishnu
 $sql1 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_DRESSES_PER_ROW'";
 $sql2 = "SELECT `value` FROM `preferences` WHERE `name`= 'NO_OF_DRESSES_TO_DISPLAY'";
-$sql3 = "SELECT `value` FROM `preferences` WHERE `name`= 'NAME_OF_FAVORITE_DRESS'";
+$sql3 = "SELECT `comments` FROM `preferences` WHERE `name`= 'NAME_OF_FAVORITE_DRESS'";
 
 $results = mysqli_query($db,$sql1);
 $results2 = mysqli_query($db,$sql2);
@@ -16,30 +16,30 @@ $results3 = mysqli_query($db,$sql3);
 
 if(mysqli_num_rows($results)>0){
     while($row = mysqli_fetch_assoc($results)){
-        $column[] = $row;
+        $row_count_array[] = $row;
     }
 }
-$rows = $column[0]['value'];
+$row_count = $row_count_array[0]['value'];
 
 if(mysqli_num_rows($results2)>0){
     while($row = mysqli_fetch_assoc($results2)){
-        $question[] = $row;
+        $dresses_count_array[] = $row;
     }
 }
-$questions = $question[0]['value'];
+$dresses_count = $dresses_count_array[0]['value'];
 
 
 if(mysqli_num_rows($results3)>0){
     while($row = mysqli_fetch_assoc($results3)){
-        $favoritedress[] = $row;
+        $xyz[] = $row;
     }
 }
-$favoritedress =$favoritedress[0]['value'];
+$fav_dress =$xyz[0]['comments'];
 ?>
 <style>#title {text-align: center;color: darkgoldenrod;}</style>
 <html>
     <head>
-        <title>QuizMaster Quiz</title>
+        <title>Project ABCD</title>
         <style>
         input {
             text-align: center;
@@ -62,18 +62,18 @@ $favoritedress =$favoritedress[0]['value'];
         </tr>
         <tr>
             <td style="width:300px">Number of Dresses Per Row:</td>
-            <td><input disabled type="int" maxlength="2" size="13" value="<?php echo $rows; ?>" title="Current value"></td> 
-            <td><input required type="int" name="new_rows" maxlength="2" size="13" title="Enter a number!"></td>
+            <td><input disabled type="int" maxlength="2" size="13" value="<?php echo $row_count; ?>" title="Current value"></td> 
+            <td><input required type="int" name="row_count" maxlength="2" size="13" title="Enter a number!"></td>
         </tr>
         <tr>
             <td style="width:300px">Number of Dresses to Display:</td>
-            <td><input disabled type="int" maxlength="2" size="13" value="<?php echo $questions; ?>" title="Current value"></td> 
-            <td><input required type="int" name="new_questions" maxlength="2" size="13" title="Enter in another number!"></td>
+            <td><input disabled type="int" maxlength="2" size="13" value="<?php echo $dresses_count; ?>" title="Current value"></td> 
+            <td><input required type="int" name="dresses_count" maxlength="2" size="13" title="Enter in another number!"></td>
         </tr>
         <tr>
             <td style="width:300px">Name of Favorite Dress:</td>
-            <td><input disabled type="text" maxlength="10" size="13" value="<?php echo $favoritedress; ?>" title="Current value"></td> 
-            <td><input required type="text" name="new_rows" maxlength="20" size="13" title="Enter your favorite dress!"></td>
+            <td><input disabled type="text" maxlength="10" size="13" value="<?php echo $fav_dress; ?>" title="Current value"></td> 
+            <td><input required type="text" name="fav_dress" maxlength="20" size="13" title="Enter your favorite dress!"></td>
         </tr>
         </table><br>
         <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Modify Preferences</button>
