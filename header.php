a6d91c0fbfd9d2381c4c30a8f048c571d7957104
Siva Jasthi <siva.jasthi@metrostate.edu>
2020-04-04 10:41:05 -0500

    delete fixes and header fix

--- header.php
+++ header.php
@@ -30,7 +30,7 @@ if(!isset($page_title)) { $page_title = 'Project ABCD'; }
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <a href="index.php" title="SILC Project ABCD"><img id="silc" src="images/about_images/abcd_logo.png"></a> 
+            <a href="index.php" title="SILC Project ABCD"><img src="images/about_images/abcd_logo.png"></a> 
             
             <!-- Login / Logout Nav menu item
                Checks if there is a valid session and if so displays "logout"
