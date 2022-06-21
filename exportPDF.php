3c66e46c9a94f70d0578bb11d031474dfc24650f
JonathanDinh1 <gn8435gu@go.minnstate.edu>
2022-06-21 18:19:54 -0500

    Update exportPDF.php

--- exportPDF.php
+++ exportPDF.php
@@ -1,32 +1,15 @@
+
 <?php
-$mysqli = mysqli_connect('localhost', 'root', '', 'abcd_db');
-$TableName = "dresses";
-
-$strSQL = "SELECT * FROM $TableName";
-$sql = mysqli_query($mysqli, $strSQL);
-
-if (mysqli_error($mysqli)) {
-    echo mysqli_error($mysqli);
-} else {
-    if($sql->num_rows > 0) {
-        $delimiter = ",";
-        $filename = "dress-data_" . date('Y-m-d') . ".pdf";
-        
-        $f = fopen('php://memory', 'w');
-        $fields = array('id', 'name', 'description', 'did_you_know', 'category', 'type', 'state_name', 'key_words', 'image_url', 'status', 'notes');
-        fputcsv($f, $fields, $delimiter);
-        
-        while($row = $sql->fetch_assoc()) {
-            $lineData = array($row['id'],$row['name'],$row['description'],$row['did_you_know'],$row['category'],$row['type'],$row['state_name'],$row['key_words'],$row['image_url'],$row['status'],$row['notes']);
-            fputcsv($f,$lineData,$delimiter);
-        }
-        fseek($f, 0);
-        
-        header('Content-Type: text/pdf');
-        header('Content-Disposition: attachment; filename="' . $filename . '";');
-        
-        fpassthru($f);
-    }
-}
-exit;
+  
+$file = $_GET["file"] .".pdf";
+  
+// We will be outputting a PDF
+header('Content-Type: application/pdf');
+  
+// It will be called downloaded.pdf
+header('Content-Disposition: attachment; filename="gfgpdf.pdf"');
+  
+$imagpdf = file_put_contents($image, file_get_contents($file)); 
+  
+echo $imagepdf;
 ?>
