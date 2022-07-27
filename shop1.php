8a68ec5c26169702e4072ecd9b009126dc4a314d
Maddy Dydyk-Compeau <105604153+maddy-dc-minnstate@users.noreply.github.com>
2022-07-26 19:54:56 -0500

    Final update - fixed responsive CSS
    resolved an error with responsive CSS where it was not reacting to page size changes.

--- shop1.php
+++ shop1.php
@@ -36,54 +36,49 @@ include('header.php');
     	height: 2749px; /* 12 items tall */
     }
     
-    @media (min-width: 506px) {
-    	.shop-box: {
-    		width: 506px;
-    	}
-    	.shop-box iframe {
-    		height: 1417px; /* 6 items tall */
-    	}
-    }
+     @media (min-width: 506px) { 
+     	.shop-box { 
+     		width: 506px; 
+     	} 
+     	.shop-box iframe { 
+     		height: 1417px; /* 6 items tall */ 
+     	} 
+     } 
     
-    @media (min-width: 746px) {
-    	.shop-box: {
-    		width: 746px;
-    	}
-    	.shop-box iframe {
-    		height: 973px; /* 4 items tall */
-    	}
-    }
+     @media (min-width: 746px) { 
+     	.shop-box { 
+     		width: 746px; 
+     	} 
+     	.shop-box iframe { 
+     		height: 973px; /* 4 items tall */ 
+     	} 
+     } 
     
-    @media (min-width: 986px) {
-    	.shop-box: {
-    		width: 986px;
-    	}
-    	.shop-box iframe {
-    		height: 751px; /* 3 items tall */
-    	}
-    }
+     @media (min-width: 986px) { 
+     	.shop-box { 
+     		width: 986px; 
+     	} 
+     	.shop-box iframe { 
+     		height: 751px; /* 3 items tall */ 
+     	} 
+     } 
     
-    @media (min-width: 1226px) {
-    	.shop-box: {
-    		width: 1226px;
-    	}
+     @media (min-width: 1226px) { 
+     	.shop-box { 
+     		width: 1226px; 
+     	}
     }
 </style>
 
 <main class="shop-page">
 	<section class="text-box">
     	<h1>This shop is found in Redbubble.</h1>
-    	<h2 id="directions">Feel free to browse/simulate a purchase. DO NOT PURCHASE YET.</h2>
+    	<h2>Feel free to browse. DO NOT PURCHASE YET.</h2>
 	</section>
 	<section class="shop-box">
 		<script type="text/javascript" src="https://www.redbubble.com/assets/external_portfolio.js"></script>
 		<script id="rb-xzfcxvzx" type="text/javascript">
-			new RBExternalPortfolio(
-				'www.redbubble.com',
-				'projectabcd',
-				3,
-				4
-			).renderIframe();
+			new RBExternalPortfolio('www.redbubble.com', 'projectabcd', 3, 4).renderIframe();
 		</script>
 	</section>
 </main>
