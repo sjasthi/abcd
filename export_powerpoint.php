2bea905f83236a3d11baf46e2dccddbbdbd46524
VianneyPham <gd3451er@go.minnstate.edu>
2022-08-09 20:41:10 -0500

    download fix

--- export_powerpoint.php
+++ export_powerpoint.php
@@ -225,8 +225,10 @@ if (mysqli_error($mysqli)) {
             }
         }
                         
-        $oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
-        $oWriterPPTX->save(getenv("HOMEDRIVE").getenv("HOMEPATH")."\Downloads" . "\sample.pptx");
+        // $oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+        // $oWriterPPTX->save(getenv("HOMEDRIVE").getenv("HOMEPATH")."\Downloads" . "\sample.pptx");
+        $oWriter = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+        $oWriter->save(__DIR__ . '/abcdsample.pptx');
     }
 }
 ?>
